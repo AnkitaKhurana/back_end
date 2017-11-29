4301fb938c86e55325f8e563d234f9d477dc52f1
Ankita Khurana <ankita0096@gmail.com>
2017-11-29 13:01:07 +0530

    db

--- db/models.js
+++ db/models.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('ChatApp'||'postgres://ogpmmliwvhlbrw:02d8a5b7c15a5ca81db6381656ed57a06a9a307735b14af1e0aa3e7cd61749f1@ec2-54-221-254-72.compute-1.amazonaws.com:5432/df1endjjfbqmth', 'root', '1234', {
     host: 'localhost',
-    dialect: 'mysql'||'postgres',
+    dialect:'postgres',
     dialectOptions: {
         ssl: true
     },
